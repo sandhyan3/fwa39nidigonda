a2cfd128c38c42caf577c7c4b5703990236c535d
sandhyan3 <S545239@NWMISSOURI.EDU>
2021-10-27 19:41:52 -0500

    updated

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var mydata = require('./routes/users');
+var mydata = require('./routes/mydata');
 
 var app = express();
 
